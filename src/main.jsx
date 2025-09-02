183bbce95dd3b4189cdbaf7a19d0612801d40bc1
williams victor <williamsvikky76@gmail.com>
2025-09-02 10:32:14 -0700

    Updated files

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// ✅ Import HashRouter
+
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* ✅ Wrap App with HashRouter */}
+
     <HashRouter>
       <App />
     </HashRouter>
