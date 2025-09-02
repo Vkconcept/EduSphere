73fd19b63b0609520abd499e32333c02936afb52
williams victor <williamsvikky76@gmail.com>
2025-09-02 10:54:05 -0700

    Fix duplicate Router issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import Home from "./Page/Home";
 import About from "./Page/About";
 import Program from "./Page/Program";
 import Admission from "./Page/Admission";
-import Contact from "./Page/contact";
+import Contact from "./Page/Contact"; 
 import Footer from "./Page/Footer";
 import Login from "./Page/Login";
 
 const App = () => {
   return (
     <>
+  
       <Navbar />
+
+     
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+
+ 
       <About />
       <Program />
       <Admission />
